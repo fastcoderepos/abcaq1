b500eecfd55fa85bf82b2d407736611d083085e9
fastCode <info@nfinityllc.com>
2021-01-10 21:59:44 +0500

    Generated with fastCode-1.0

---
+++ abcaq1Client/src/app/core/main-nav/entities.ts
@@ -0,0 +1,27 @@
+export var Entities: string[] = [
+		"category",
+	"address",
+	"filmactor",
+	"film",
+	"customer",
+	"staff",
+	"store",
+	"city",
+	"payment",
+	"rental",
+	"language",
+	"actor",
+	"country",
+	"filmcategory",
+	"inventory",
+]
+
+export var AuthEntities: string[] = [
+	"user",
+	"role",
+	"permission",
+	"rolepermission",
+	"userpermission",
+	"userrole",
+]
+
